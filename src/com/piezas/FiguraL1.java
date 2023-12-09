02a224ffab8dca61eb0a867f38ed72d1df0b5002
felix2680 <feliciano.hernandez@alumno.udg.mx>
2023-12-09 14:00:34 -0600

    la primer figura ya se puede girar

--- src/com/piezas/FiguraL1.java
+++ src/com/piezas/FiguraL1.java
@@ -7,11 +7,11 @@ import java.awt.Color;
  * @author felix
  */
 public class FiguraL1 extends Figura {
-    
+
     public FiguraL1() {
         super.crearFigura(Color.orange);
     }
-    
+
     @Override
     public void setXY(int x, int y) {
         bloques[0].posX = x;
@@ -23,21 +23,21 @@ public class FiguraL1 extends Figura {
         bloques[3].posX = bloques[0].posX + Bloque.TAMANO;
         bloques[3].posY = bloques[0].posY + Bloque.TAMANO;
     }
-    
+
     @Override
     public void direccion1() {
         bloquesTemp[0].posX = bloques[0].posX;
         bloquesTemp[0].posY = bloques[0].posY;
         bloquesTemp[1].posX = bloques[0].posX;
-        bloquesTemp[1].posY = bloques[0].posY + Bloque.TAMANO;
+        bloquesTemp[1].posY = bloques[0].posY - Bloque.TAMANO;
         bloquesTemp[2].posX = bloques[0].posX;
-        bloquesTemp[2].posY = bloques[1].posY + Bloque.TAMANO;
+        bloquesTemp[2].posY = bloques[0].posY + Bloque.TAMANO;
         bloquesTemp[3].posX = bloques[0].posX + Bloque.TAMANO;
-        bloquesTemp[3].posY = bloques[2].posY;
-        
+        bloquesTemp[3].posY = bloques[0].posY + Bloque.TAMANO;
+
         girar(1);
     }
-    
+
     @Override
     public void direccion2() {
         bloquesTemp[0].posX = bloques[0].posX;
@@ -48,16 +48,35 @@ public class FiguraL1 extends Figura {
         bloquesTemp[2].posY = bloques[0].posY;
         bloquesTemp[3].posX = bloques[0].posX - Bloque.TAMANO;
         bloquesTemp[3].posY = bloques[0].posY + Bloque.TAMANO;
-        
+
         girar(2);
     }
-    
+
     @Override
     public void direccion3() {
-        
+        bloquesTemp[0].posX = bloques[0].posX;
+        bloquesTemp[0].posY = bloques[0].posY;
+        bloquesTemp[1].posX = bloques[0].posX;
+        bloquesTemp[1].posY = bloques[0].posY + Bloque.TAMANO;
+        bloquesTemp[2].posX = bloques[0].posX;
+        bloquesTemp[2].posY = bloques[0].posY - Bloque.TAMANO;
+        bloquesTemp[3].posX = bloques[0].posX - Bloque.TAMANO;
+        bloquesTemp[3].posY = bloques[0].posY - Bloque.TAMANO;
+
+        girar(3);
     }
-    
+
     @Override
     public void direccion4() {
+        bloquesTemp[0].posX = bloques[0].posX;
+        bloquesTemp[0].posY = bloques[0].posY;
+        bloquesTemp[1].posX = bloques[0].posX - Bloque.TAMANO;
+        bloquesTemp[1].posY = bloques[0].posY;
+        bloquesTemp[2].posX = bloques[0].posX + Bloque.TAMANO;
+        bloquesTemp[2].posY = bloques[0].posY;
+        bloquesTemp[3].posX = bloques[0].posX + Bloque.TAMANO;
+        bloquesTemp[3].posY = bloques[0].posY - Bloque.TAMANO;
+        
+        girar(4);
     }
 }
